02843fd3ab7dfe3f7532bb83d2de5efe8ec80082
tient-anh <79672556+tient-anh@users.noreply.github.com>
2021-02-26 17:48:55 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,44 +1,41 @@
-const express = require('express')
-const expressLayouts = require('express-ejs-layouts')
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(express.static('public'))
-app.use('/css', express.static(__dirname + 'public/css'))
+app.use(express.static(__dirname + '/public'));
 
-
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.redirect("/login")
-  })
+  res.redirect('/login');
+});
 
 app.get('/login', (req, res) => {
-    res.render("login.ejs", { title: "Login PAGE" })
-  })
+  res.render('login.ejs', { title: 'Login' });
+});
 
 app.get('/register', (req, res) => {
-    res.render("register.ejs", { title: "Register PAGE" })
-  })
+  res.render('register.ejs', { title: 'Register' });
+});
 
-  app.get('/profile', (req, res) => {
-    res.render("profile.ejs", { title: "Login PAGE" })
-  })
+app.get('/profile', (req, res) => {
+  res.render('profile.ejs', { title: 'Profile' });
+});
 
-app.get('/form', (req, res) => {
-    res.render("form.ejs", { title: "Register PAGE" })
-  })
+app.get('/quote', (req, res) => {
+  res.render('quote.ejs', { title: 'Quote' });
+});
 
-  app.get('/result', (req, res) => {
-    res.render("result.ejs", { title: "Login PAGE" })
-  })
+app.get('/quotehistory', (req, res) => {
+  res.render('quotehistory.ejs', { title: 'Quote History' });
+});
 
-
-app.listen(port, function(error){
-    if (error){
-        console.log('went wrong',error)
-    } else {
-        console.log('server is good ' + port)
-    }
-})
+app.listen(port, function (error) {
+  if (error) {
+    console.log('went wrong', error);
+  } else {
+    console.log('server is good ' + port);
+  }
+});
