424a5762908bd71201a2a2d8b233ddc94465e845
BachHoang1 <71099318+BachHoang1@users.noreply.github.com>
2021-04-02 22:33:08 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,158 +1,52 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
-
 const express = require('express');
-const app = express();
-const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const LocalStrategy = require('passport-local').Strategy;
 const passport = require('passport');
-const flash = require('express-flash');
-const session = require('express-session');
+const passportLocalMongoose = require('passport-local-mongoose');
+const cookieParser = require('cookie-parser');
+const Client = require('./models/client');
+const Quote = require('./models/quote');
+const URL = process.env.MONGODB_URI || "mongodb://localhost/server";
+const PORT = process.env.PORT || 3000;
 
-const initializePassport = require('./passport-config');
-initializePassport(
-  passport,
-  (email) => users.find((user) => user.email === email),
-  (id) => users.find((user) => user.id === id)
-);
+var messages = [];
 
-const users = [];
+mongoose.connect(URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+}).then(() => {
+    console.log("Connected to client database on MongoDB Atlas server.");
+}).catch(err => {
+    console.log('ERROR:', err.message);
+});
 
-app.set('view-engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
-app.use(express.urlencoded({ extended: false }));
-app.use(flash());
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(require("express-session")({
+    secret: "This is a secret",
     resave: false,
-    saveUninitialized: false,
-  })
-);
+    saveUninitialized: true
+}));
 app.use(passport.initialize());
 app.use(passport.session());
-
-//Route History
-app.get('/', checkAuthenticated, (req, res) => {
-  if (req.user.isFirst) {
-    req.user.isFirst = false;
-    return res.redirect('/profile');
-  }
-  res.render('quotehistory.ejs', {
-    title: 'History',
-    history: req.user.history,
-  });
+app.use(cookieParser('secret'));
+app.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    next();
 });
 
-// Quote
-app.get('/quote', checkAuthenticated, (req, res) => {
-  res.render('quote.ejs', { title: 'Quote', user: req.user });
-});
+passport.use(new LocalStrategy(Client.authenticate()));
+passport.serializeUser(Client.serializeUser());
+passport.deserializeUser(Client.deserializeUser());      
 
-app.post('/quote', checkAuthenticated, (req, res) => {
-  userprofile = req.user.profile;
-  try {
-    req.user.history.push({
-      gallons: req.body.gallons,
-      deliveryAddress: `${userprofile.address}, ${userprofile.city} ${userprofile.state} ${userprofile.zipcode}`,
-      deliveryDate: req.body.deliveryDate,
-      suggestedPrice: req.body.suggestedPrice,
-      amountDue: 0.01,
-    });
-    res.redirect('/');
-  } catch (e) {
-    res.redirect('/quote');
-  }
-});
+passport.use(new LocalStrategy(Quote.authenticate()));
+passport.serializeUser(Quote.serializeUser());
+passport.deserializeUser(Quote.deserializeUser());      
 
-// Profile Route
-app.get('/profile', checkAuthenticated, (req, res) => {
-  res.render('profile.ejs', { title: 'Profile', profile: req.user.profile });
-});
+app.use('/', require('./routes/index'));
+app.use('/clients', require('./routes/clients'));
 
-app.post('/profile', checkAuthenticated, (req, res) => {
-  userprofile = req.user.profile;
-  try {
-    (req.user.profile = {
-      name: req.body.name,
-      address: req.body.address,
-      address2: req.body.address2,
-      city: req.body.city,
-      state: req.body.state,
-      zipcode: req.body.zipcode,
-    }),
-      res.redirect('/');
-  } catch (e) {
-    res.redirect('/profile');
-  }
-});
-
-// Login Route
-app.get('/login', checkNotAuthenticated, (req, res) => {
-  res.render('login.ejs', { title: 'Login' });
-});
-
-app.post(
-  '/login',
-  checkNotAuthenticated,
-  passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login',
-    failureFlash: true,
-  })
-);
-
-// Register Route
-app.get('/register', checkNotAuthenticated, (req, res) => {
-  res.render('register.ejs', { title: 'Register' });
-});
-
-app.post('/register', checkNotAuthenticated, async (req, res) => {
-  try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    users.push({
-      id: Date.now().toString(),
-      email: req.body.email,
-      password: hashedPassword,
-      isFirst: true,
-      profile: {
-        name: req.body.name,
-        address: '',
-        address2: '',
-        city: '',
-        state: '',
-        zipcode: '',
-      },
-      history: [],
-    });
-    res.redirect('/login');
-  } catch (e) {
-    res.redirect('/register');
-  }
-});
-
-// Logout Route
-app.get('/logout', checkAuthenticated, (req, res) => {
-  req.logOut();
-  res.redirect('/login');
-});
-
-// Check Authenticated
-function checkAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-
-  res.redirect('/login');
-}
-
-function checkNotAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return res.redirect('/');
-  }
-  next();
-}
-
-app.listen(process.env.PORT, () =>
-  console.log(`Server Up and running @ port: ${process.env.PORT}`)
-);
+app.listen(PORT, () => { console.log('FuelQuote server listening on port ' + PORT + '.') });
