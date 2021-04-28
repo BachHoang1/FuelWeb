6a0eb4ae696a96d2b44fe3859d1f418f66d32d0e
BachHoang1 <71099318+BachHoang1@users.noreply.github.com>
2021-04-28 08:27:26 -0500

    Add files via upload

--- js/price.js
+++ js/price.js
@@ -1,7 +1,7 @@
 const tools = require('../js/tools.js');
 
 module.exports = {
-    getSuggestedPrice: function(loc, mon, qh, gal) {
+    getSuggestedPrice: function(loc, qh, gal) {
         let locFactor    = 0.0;
         let rhFactor     = 0.0;
         let galFactor    = 0.0;
