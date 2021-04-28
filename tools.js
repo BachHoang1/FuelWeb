ed7d28c384d7a11d5d0638caa7d78e235d91e444
BachHoang1 <71099318+BachHoang1@users.noreply.github.com>
2021-04-28 14:39:40 -0500

    Delete tools.js

--- tools.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    inState: function(state) {
-        return state === 'TX' ? true : false;
-    },
-    hasHistory: function(quoteHistory) {
-        console.log(quoteHistory.length)
-        return quoteHistory.length != 0 ? true : false;
-    },
-};
