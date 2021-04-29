4cd883b810ddf789ffa8351eb46c0d66293c0074
BachHoang1 <71099318+BachHoang1@users.noreply.github.com>
2021-04-28 23:21:21 -0500

    Delete register.test.js

--- test/register.test.js
+++
@@ -1,7 +0,0 @@
-var request = require('supertest'), app = require('../server')
-
-describe("register", function (){
-    it("wellcomes the user", function (done){
-        request(app).get("/").expect(3000)
-    })
-})
